a1214ed0fe73f47baf2b9e736ff267005e4e9125
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-12 21:17:03 -0500

    beginning to make .js organized and purposeful with functions to target each of the various weather aspects

--- j.js
+++ j.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     //.ready basically lets jQuery run when the page has loaded and when everything's ready...
 
     const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
@@ -10,20 +10,23 @@ $(document).ready(function () {
 
     $(".search-button").on("click", function () {
         var searchValue = $("#search-value").val()
-        console.log(searchValue)
-        //search weather function here (search value)
+
+        Temperature(searchValue)
+        wind(searchValue)
+        getUV(response.coord.lat, response.coord.lon)
+
     })
 //__________________________________________________________________________________________________
 
 //__________________________________________________________________________________________________
 
-    function currentWeather(searchValue) {
+    function Temperature(searchValue) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c",
             type: "GET"
         }).then(function (response) {
             console.log(response)
-            var title = $(".today").html("<h1>" + response.name + " Weather Details</h1>" + "Temperature" + response.main.temp);
+            var title = $(".today").html("<h1>" + response.name + " Current Weather Details</h1>" + "Temperature" + response.main.temp);
             //THIS WORKS ^ refer to this!!
         })
     }
@@ -31,33 +34,33 @@ $(document).ready(function () {
 
 //__________________________________________________________________________________________________
 
-    function getForecast(searchValue) {
+    function wind(searchValue) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c",
             type: "GET"
         }).then(function (response) {
             console.log(reponse)
-            $(".forecast").html("<p>Feels like" + response.list[0].main.feels_like + "</p>");
+            $(".windspeed").html("<p>Wind speed: " + response.wind.speed + "</p>");
         })
     }
 //__________________________________________________________________________________________________
 
 //__________________________________________________________________________________________________
 
-    function getUV(lat, lon) {
+    function getUV(lat,lon) {
         $.ajax({
             ulr: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c" + "c&lat" + lat + "&lon" + lon,
             type: "GET"
         }).then(function (response) {
             console.log(response)
-            $(".uv").html("<p>uv" + response.value + "</p>");
+            $(".uv").html("<p>uv index:" + response.value + "</p>");
         })
     }
 //__________________________________________________________________________________________________
 
 //__________________________________________________________________________________________________
 
-currentWeather(searchValue)
-getForecast(searchValue)
+Temperature(searchValue)
+feelsLike(searchValue)
 getUV(response.coord.lat, response.coord.lon)
 })
