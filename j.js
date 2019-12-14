4288afe5c2b247e0ab745f99249bda1aba3e4927
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-14 14:37:27 -0500

    cleaned up my code, however I still need to figure out how to get the variables 'lat' and 'lon' to retrieve the latitute and longitudinal coordinates of the place typed into the search field

--- j.js
+++ j.js
@@ -1,40 +1,40 @@
 $(document).ready(function() {
-    //.ready basically lets jQuery run when the page has loaded and when everything's ready...
-
-    const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
-    const weatherApiKey = "&APPID=2e7aa3ec88b7d2439a8b916c6274910c"
-    //our API URL and key.
 
     //on-click + search field function.
     $(".search-button").on("click", function() {
         var searchValue = $("#search-value").val()
 
-        Temperatureandwindandicon(searchValue)
-
+        Temperatureandwindandhumidity(searchValue)
     })
 
     //From this API we're pulling info on temperature, windspeed, and humidity.
-    function Temperatureandwindandicon(searchValue) {
+    function Temperatureandwindandhumidity(searchValue) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c",
             type: "GET"
         }).then(function (response) {
             console.log(response)
             $(".temperature").html("<h1>" + response.name + " Current Weather Details</h1>" + "Temperature: " + response.main.temp);
-            //THIS WORKS ^ refer to this!!
 
             $(".windspeed").html("<p> Wind speed: </p>" + response.wind.speed);
-            //^This SHOULD display the wind speed.
 
             $(".humidity").html("<p> Humidity: </p>" + response.main.humidity);
+        })
+        }
+    })
 
+    // Since the above API doesn't offer information on UV index or a 5-day forecast, we're referring to a second one below. (DarkSky.)
+
+    var darkkey = "9f68d2be222dbb1a918e1f5457bf6724/"
+    var lat = 
+    var lon = 
+
+    function getUV(lat,lon) {
+
+        $.ajax({
+            url: "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkkey + lat + "," + lon,
+            type: "GET"
+        }).then(function(moreinfo) {
+            console.log(moreinfo);
         })
     }
-})
-//_________________________________________
-$.ajax({
-    url: "https://weatherbit-v1-mashape.p.rapidapi.com",
-    method: "GET"
-}).then(function(BBeight) {
-    console.log(BBeight);
-})
