26eee8cc4085bfed497f98bf4a2b082cf9cc0b6c
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-14 13:40:26 -0500

    have wind speed, temperature and humidity being displayed, also tinkered with CSS

--- j.js
+++ j.js
@@ -4,63 +4,37 @@ $(document).ready(function() {
     const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q="
     const weatherApiKey = "&APPID=2e7aa3ec88b7d2439a8b916c6274910c"
     //our API URL and key.
-//__________________________________________________________________________________________________
 
-//__________________________________________________________________________________________________
-
-    $(".search-button").on("click", function () {
+    //on-click + search field function.
+    $(".search-button").on("click", function() {
         var searchValue = $("#search-value").val()
 
-        Temperature(searchValue)
-        wind(searchValue)
-        getUV(response.coord.lat, response.coord.lon)
+        Temperatureandwindandicon(searchValue)
 
     })
-//__________________________________________________________________________________________________
 
-//__________________________________________________________________________________________________
-
-    function Temperature(searchValue) {
+    //From this API we're pulling info on temperature, windspeed, and humidity.
+    function Temperatureandwindandicon(searchValue) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c",
             type: "GET"
         }).then(function (response) {
             console.log(response)
-            var title = $(".today").html("<h1>" + response.name + " Current Weather Details</h1>" + "Temperature" + response.main.temp);
+            $(".temperature").html("<h1>" + response.name + " Current Weather Details</h1>" + "Temperature: " + response.main.temp);
             //THIS WORKS ^ refer to this!!
+
+            $(".windspeed").html("<p> Wind speed: </p>" + response.wind.speed);
+            //^This SHOULD display the wind speed.
+
+            $(".humidity").html("<p> Humidity: </p>" + response.main.humidity);
+
         })
     }
-//__________________________________________________________________________________________________
-
-//__________________________________________________________________________________________________
-
-    function wind(searchValue) {
-        $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c",
-            type: "GET"
-        }).then(function (response) {
-            console.log(reponse)
-            $(".windspeed").html("<p>Wind speed: " + response.wind.speed + "</p>");
-        })
-    }
-//__________________________________________________________________________________________________
-
-//__________________________________________________________________________________________________
-
-    function getUV(lat,lon) {
-        $.ajax({
-            ulr: "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=2e7aa3ec88b7d2439a8b916c6274910c" + "c&lat" + lat + "&lon" + lon,
-            type: "GET"
-        }).then(function (response) {
-            console.log(response)
-            $(".uv").html("<p>uv index:" + response.value + "</p>");
-        })
-    }
-//__________________________________________________________________________________________________
-
-//__________________________________________________________________________________________________
-
-Temperature(searchValue)
-feelsLike(searchValue)
-getUV(response.coord.lat, response.coord.lon)
+})
+//_________________________________________
+$.ajax({
+    url: "https://weatherbit-v1-mashape.p.rapidapi.com",
+    method: "GET"
+}).then(function(BBeight) {
+    console.log(BBeight);
 })
